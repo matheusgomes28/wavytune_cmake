89426d856ad53c9bde29130e1e05fec65632ab49
Matheus Gomes <mgomes1@jaguarlandrover.com>
2020-07-16 14:12:01 +0100

    Added a test suite and the conanfile with dependencies.

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile
+
+class WavyTuneConan(ConanFile):
+
+	settings = ("os", "compiler", "build_type", "arch")
+	generators = ("cmake_find_package")
+	requires = (
+		"glew/2.1.0@bincrafters/stable",
+		"glfw/3.3.2",
+		"glm/0.9.9.8",
+		"gtest/1.10.0",
+		"portaudio/v190600.20161030@bincrafters/stable",
+		)
+
+
+	def imports(self):
+		self.copy("*.dll", dst="bin", src="bin")
+		self.copy("*.dylib", dst="bin", src="lib")
