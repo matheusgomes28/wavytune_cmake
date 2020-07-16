9bddd4ef7b89e92c4cd3863bd401d40b9eb7c31b
Matheus Gomes <mgomes1@jaguarlandrover.com>
2020-07-16 14:08:34 +0100

    Added the cmake files for a simple main compilation

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+// #include <data_structures/byte_array.h>
+#include <fourier/matrix.h>
+// #include <gl_abstractions/vao.h>
+#include <iostream>
+
+int main()
+{
+	// VAO vao{"vao testing"};
+	Matrix<double> hello{2,2};
+	// ByteArray hello2(20);
+
+  std::cout << "Hello World!" << std::endl;
+	return 0;
+}
